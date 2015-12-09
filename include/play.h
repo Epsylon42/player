383990b9a152b791d6e6c881c7ef65988351ec9e
Epsylon <eepsylon.3@gmail.com>
2015-12-09 16:55:20 +0200

    Improved playback functionality.

--- include/play.h
+++ include/play.h
@@ -6,3 +6,4 @@
 #include <ao/ao.h>
 
 void play(Track* track);
+void playPacket(AVPacket* packet, ao_device* device, Track* track);
--- src/play.cpp
+++ src/play.cpp
@@ -4,7 +4,6 @@ void play(Track* track)
 {
    //av_dump_format(track->container, 1, track->filePath.c_str(), 0);
    
-   //ao_sample_format* sampleFormat = getSampleFormat(track);
    ao_device* device = ao_open_live(ao_default_driver_id(), track->sampleFormat, NULL);
    if (device == NULL)
    {
@@ -24,32 +23,39 @@ void play(Track* track)
       }
       if (packet->stream_index != track->streamID)
       {
+	 av_packet_unref(packet);
 	 delete packet;
-	 printf("Packet from invalid stream\n");
+	 //printf("Packet from invalid stream\n");
 	 continue;
-      }
-      AVFrame* frame;
-      frame = decodeFrame(track, track->sampleFormat, packet);
-      if (frame == NULL)
-      {
-	 continue;
-      }
-      
-      int dataSize =
-	 av_samples_get_buffer_size(NULL,
-				    track->codecContext->channels,
-				    frame->nb_samples,
-				    track->codecContext->sample_fmt,
-				    1);
-      for (int i = 0; i < track->sampleFormat->channels; i++)
-      {
-	 ao_play(device, reinterpret_cast<char*>(frame->extended_data[i]), frame->linesize[i]);
       }
 
+      playPacket(packet, device, track);
+      
       av_packet_unref(packet);
       delete packet;
-      av_frame_unref(frame);
-      delete frame;
    }
 }
 
+void playPacket(AVPacket* packet, ao_device* device, Track* track)
+{
+   AVFrame* frame;
+   frame = decodeFrame(track, track->sampleFormat, packet);
+   if (frame == NULL)
+   {
+      return;
+   }
+      
+   int dataSize =
+      av_samples_get_buffer_size(NULL,
+				 track->codecContext->channels,
+				 frame->nb_samples,
+				 track->codecContext->sample_fmt,
+				 1);
+   for (int i = 0; i < track->sampleFormat->channels; i++)
+   {
+      ao_play(device, reinterpret_cast<char*>(frame->extended_data[i]), frame->linesize[i]);
+   }
+
+   av_frame_unref(frame);
+   delete frame;
+}
